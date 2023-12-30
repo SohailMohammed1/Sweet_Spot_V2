88dd73b0fedc280c8da1b48d163217f24cb6d5b9
SohailMohammed1 <sohailm2233@gmail.com>
2023-12-30 00:34:38 +0000

    Finally added all of the necessary logic for a basic login structure

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
+from SweetSpot.models import DinnerReservation
+from django.contrib.auth.decorators import login_required
 
 def signup(request):
     if request.method == 'POST':
@@ -13,4 +15,13 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
+def account_options(request):
+    return render(request, 'accounts/options.html')
 
+def profile(request):
+    return render(request, 'accounts/profile.html')
+
+@login_required
+def reservations_list(request):
+    user_reservations = Reservation.objects.filter(user=request.user)
+    return render(request, 'SweetSpot/Sweet_Spot.html', {'reservations': user_reservations})
--- django_SweetSpot/urls.py
+++ django_SweetSpot/urls.py
@@ -32,6 +32,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('signup/', account_views.signup, name='signup'),
+    path('profile/', account_views.profile, name='profile'),
+    path('account-options/', account_views.account_options, name='account_options'),
+
 ]
 
 
