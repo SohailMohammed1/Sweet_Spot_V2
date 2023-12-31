0117388c00142c888ee9d0e1cdc692e1ee805e5f
SohailMohammed1 <sohailm2233@gmail.com>
2023-12-31 00:52:38 +0000

    testing merging heroku and github together

--- django_SweetSpot/settings.py
+++ django_SweetSpot/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q8)o=cs-gujy%6@$r*imt$chtyu^vn)8pi$-_mrpm_ntb$985_'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-q8)o=cs-gujy%6@$r*imt$chtyu^vn)8pi$-_mrpm_ntb$985_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sohailm1-django-sweetspot-app-f241a1c4a62e.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
